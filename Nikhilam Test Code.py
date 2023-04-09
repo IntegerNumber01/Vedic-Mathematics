0467cf5abe1df32695d0c110fce15ca207235f57
Integer Number <124754713+IntegerNumber01@users.noreply.github.com>
2023-04-09 08:05:56 -0700

    Create Nikhilam Test Code.py
    This is the test code for the Nikhilam Sutra program. The first set of code can be used for manually entering numbers into the num1 and num2 variables to check if you get a valid answer. You can get a recursion error, a solution that is not correct, or a right answer. The second set is a piece of code that will search an extensive range of equations to check how many it can solve. It will return the amount it solved, and you can compare that with the total number of equations by multiplying the two ranges. This piece of code can be modified to fit unique needs.

---
+++ Nikhilam Test Code.py
@@ -0,0 +1,28 @@
+## Manual Testing ##
+num1 =
+num2 =
+
+try:
+    ANS = main(num1, num2)
+    print('ANSWER:  ', ANS)
+    print('IS IT CORRECT?', int(ANS) == num1 * num2)
+except RecursionError:
+    print('ERROR: ', num1, 'x', num2)
+
+
+## Exahustive check for equations solved ##
+count = 0
+for i in range(10):
+    for x in range(10):
+        try:
+            ANS = main(x, i)
+            if ANS == 'Formula Not Applicable':
+                pass
+            elif not ANS == x * i:
+                print('NOT EQUAL', x, 'x', i, ANS)
+            else:
+                count += 1
+        except RecursionError:
+            print('ERROR: ', x, 'x', i)
+
+print(count, 'equations solved')
