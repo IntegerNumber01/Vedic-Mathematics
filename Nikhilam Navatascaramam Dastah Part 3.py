fa8e96a8d70e2dfb31812801384469c05abfe06a
Integer Number <124754713+IntegerNumber01@users.noreply.github.com>
2024-01-26 18:21:53 -0800

    Added comments

--- Nikhilam Navatascaramam Dastah Part 3.py
+++ Nikhilam Navatascaramam Dastah Part 3.py
@@ -1,4 +1,8 @@
-def main(num1, num2):
+def main(num1=int, num2=int):
+    # First priority conditions:
+    # 1. If any number is equal to 0, return 0
+    # 2. If any number is equal to 1, return the other number
+    # 3. If both numbers are less than equal to 5, use the multiple table
     if num1 == 0 or num2 == 0:
         return 0
 
@@ -11,25 +15,33 @@ def main(num1, num2):
         return multiple_table[num1][num2]
 
     if num2 < num1:
+        # Swap num1 and num2 so that num1 will always be less than num2
         num1, num2 = num2, num1
 
+    # Determine bases for both numbers
+    # Using the minus method
     base1 = int('1' + ('0' * len(str(num1))))
+    # Using the plus method
     base2 = int('1' + ('0' * (len(str(num2)) - 1)))
 
     if not base1 == base2:
         return 'Formula Not Applicable'
 
+    # Numbers cannot be a multiple of 10
     if num1 % 10 == 0 or num2 % 10 == 0:
         return 'Formula Not Applicable'
 
     zeros = len(str(base1)) - 1
 
+    # First step is to figure out the right values
     right1 = base1 - num1
     right2 = num2 - base2
 
+    # Getting the left and right portions of the final answer
     left_ans = num1 + right2
     right_ans = main(right1, right2)
 
+    # Becuase of recursion, right_ans can end up being 'Formula Not Applicable'
     if right_ans == 'Formula Not Applicable':
         return 'Formula Not Applicable'
 
